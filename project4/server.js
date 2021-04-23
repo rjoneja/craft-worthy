dde96c1742b32b42e8b715fdfa25597e5ea0e6e4
rjoneja <34627053+rjoneja@users.noreply.github.com>
2021-04-23 01:35:48 +0100

    Add files via upload

---
+++ project4/server.js
@@ -0,0 +1,10 @@
+console.log('Hello from the server')
+
+// Load Node modules
+var express = require('express');
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Port website will run on
+app.listen(8080);
